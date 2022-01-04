dbc73b635e4a6dcb0884c494337f3ac754892995
James Hall <jdhall@live.co.uk>
2022-01-04 19:36:52 +0800

    initial commit

---
+++ src/lib/Card.svelte
@@ -0,0 +1,24 @@
+<!-- <script context="module">
+	let question;
+	let choices;
+</script>
+
+<script>
+</script> -->
+
+<main class=" space-y-8 text-center border h-80 w-1/2 place-content-center place m-auto">
+	<div>
+		<p>question 1/10</p>
+	</div>
+	<div>
+		<h3 class="text-center text-xl">This is the question you have to answer</h3>
+	</div>
+	<div
+		class="bg-sky-400 grid grid-cols-2 place-content-center place-items-center text-center w-2/3 h-1/2 gap-4 m-auto "
+	>
+		<div class="shadow w-40 bg-slate-300 border hover:shadow-2xl h-10 ">answer 1</div>
+		<div class="w-40 bg-slate-300 border hover:shadow-2xl h-10">answer 1</div>
+		<div class="w-40 bg-slate-300 border hover:shadow-2xl h-10">answer 1</div>
+		<div class="w-40 bg-slate-300 border hover:shadow-2xl h-10">answer 1</div>
+	</div>
+</main>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<slot />
+
+<style global lang="postcss">
+	@import url('https://fonts.googleapis.com/css?family=Nunito|Quicksand');
+	@import url('https://fonts.googleapis.com/css?family=Nunito|Raleway');
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
